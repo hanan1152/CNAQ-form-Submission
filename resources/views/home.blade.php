7d73395507328ab9d238a36ef1326b880da245a1
hanan1152 <77604157+hanan1152@users.noreply.github.com>
2021-02-21 18:43:55 +0300

    created css,add home page,footer and nav bar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+@extends('layout')
+
+@section('content')
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+<h1> Home page </h1>
+<p> Please search for your grades form the nav bar </p>
+    
+</body>
+</html>
+
+@endsection
